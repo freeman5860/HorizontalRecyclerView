c0b4805b95b62321914b48450895666f0f226547
alberthe(何金源) <alberthe@tencent.com>
2017-01-10 15:27:13 +0800

    create origin project

---
+++ HorizontalRecyclerView/app/src/main/java/com/freeman/horizontalrecyclerview/DemoActivity.java
@@ -0,0 +1,13 @@
+package com.freeman.horizontalrecyclerview;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DemoActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo);
+    }
+}
